8392c2b5bdf7227fb29e5e86d12bac13ffa29df5
gillyspy <gillyspy@gmail.com>
2020-10-29 23:20:31 -0400

    refactor requesthandler

--- app.js
+++ app.js
@@ -2,40 +2,8 @@ const { RSA_NO_PADDING } = require("constants");
 const fs = require("fs");
 const http = require("http");
 const { parse } = require("path");
+const routes = require("./routes");
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  let body = "";
-  if (url === "/") {
-    body = `<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>`;
-  } else if (url === "/message" && method === "POST") {
-    const bodychunk = [];
-    req.on("data", (chunk) => {
-      //console.log(chunk)
-      bodychunk.push(chunk);
-    });
-    return req.on("end", () => {
-      const parsedBody = Buffer.concat(bodychunk).toString();
-      console.log(parsedBody);
-      const message = parsedBody.split("=")[1];
-      fs.writeFile("message.txt", message, (err) => {
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
-      });
-    });
-  } else {
-    body = "<body><h1>Hello</h1></body>";
-  }
-
-  if (method === "GET") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write(body);
-    res.write("</html");
-  }
-  res.end(); //process.exit()
-});
+const server = http.createServer(routes);
 
 server.listen(3000);
---
+++ routes.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+
+const requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+  let body = "";
+  if (url === "/") {
+    body = `<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>`;
+  } else if (url === "/message" && method === "POST") {
+    const bodychunk = [];
+    req.on("data", (chunk) => {
+      //console.log(chunk)
+      bodychunk.push(chunk);
+    });
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(bodychunk).toString();
+      console.log(parsedBody);
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+  } else {
+    body = "<body><h1>Hello</h1></body>";
+  }
+  if (method === "GET") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write(body);
+    res.write("</html");
+  }
+  res.end(); //process.exit()
+};
+
+module.exports = requestHandler;
