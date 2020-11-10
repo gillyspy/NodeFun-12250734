93839e4fda32876928bfb17e9d92fca638e879bf
gillyspy <gillyspy@gmail.com>
2020-11-10 11:00:35 -0500

    using multiple apps

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-const http = require("http");
-
 const express = require('express');
+
 const app = express();
 
-app.use( (req, res, next)=>{
-    console.log('in the middleware!');
-    next();
-})
-
-app.use( (req, res, next)=>{
+app.use('/', (req,res,next)=>{
+    console.log('this always runs');
+    next()
+});
+app.use('/add-product', (req, res, next)=>{
+    console.log('perhaps adding product in the database?');
+    res.send('in product');
+});
+app.use( '/',(req, res, next)=>{
     console.log('in another middleware!');
     res.send('<h1>Hello from Express</h1>');
-})
+});
+
+
 
 app.listen(3000);
