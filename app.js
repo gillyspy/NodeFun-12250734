f13106a4c56f60ff99cfde247c6ad1e78ac04ac9
gillyspy <gillyspy@gmail.com>
2020-11-10 11:46:29 -0500

    slightly more elegant admin routing

--- app.js
+++ app.js
@@ -8,8 +8,12 @@ const shopRoutes = require('./routes/shop.js');
 
 app.use(bodyParser.urlencoded({extended : false}));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
+app.use(shopRoutes)
 
-app.use(shopRoutes);
+app.use((req,res,next)=>{
+   console.log('404?');
+   res.status(404).send('<h1>404 dude</h1>');
+});
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -4,9 +4,9 @@ const router = express.Router();
 
 router.get('/add-product', (req, res, next)=>{
     console.log('add-product');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">add product</button></form>');
+    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">add product</button></form>');
 });
-router.post('/product', (req, res, next)=>{
+router.post('/add-product', (req, res, next)=>{
     console.log('perhaps adding product in the database?', req.body);    
     res.redirect('/')
 });
