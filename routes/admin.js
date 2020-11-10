44fb7953acf189b3d5265e1d77b8919c8f9a076d
gillyspy <gillyspy@gmail.com>
2020-11-10 12:01:28 -0500

    adding views for shop and admin and MVC'ing

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,11 @@
+const path = require('path');
 const express = require('express')
 
 const router = express.Router();
 
 router.get('/add-product', (req, res, next)=>{
     console.log('add-product');
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">add product</button></form>');
+    res.sendFile( path.join(__dirname,'../','views','add-product.html'));
 });
 router.post('/add-product', (req, res, next)=>{
     console.log('perhaps adding product in the database?', req.body);    
