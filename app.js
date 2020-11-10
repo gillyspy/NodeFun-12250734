81992d926a962b1d6449eeff9dd4e47a5318a7ab
gillyspy <gillyspy@gmail.com>
2020-11-10 10:49:56 -0500

    experimenting with Express

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-const { RSA_NO_PADDING } = require("constants");
-const fs = require("fs");
 const http = require("http");
-const { parse } = require("path");
-const routes = require("./routes.js");
 
-const server = http.createServer(routes);
+const express = require('express');
+const app = express();
+
+app.use( (req, res, next)=>{
+    console.log('in the middleware!');
+    next();
+})
+
+app.use( (req, res, next)=>{
+    console.log('in another middleware!');
+    res.send('<h1>Hello from Express</h1>');
+})
+
+const server = http.createServer(app);
+
+
+
 server.listen(3001);
