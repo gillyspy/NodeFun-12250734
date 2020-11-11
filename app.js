015297c4ea0070abf74e39593083768396f3358f
gillyspy <gillyspy@gmail.com>
2020-11-10 20:19:12 -0500

    fix route source for 404

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(shopRoutes);
 
 app.use((req, res, next) => {
   console.log("404");
-  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
+  res.status(404).render("404");
 });
 
 app.listen(3000);
