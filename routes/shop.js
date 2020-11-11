20ea0e38e52a57029236c73a56ada7638cb97fd4
gillyspy <gillyspy@gmail.com>
2020-11-11 07:56:51 -0500

    make hbs layout template

--- routes/shop.js
+++ routes/shop.js
@@ -7,14 +7,13 @@ const adminData = require("./admin");
 const router = express.Router();
 router.get("/", (req, res, next) => {
   const products = adminData.products;
-  const shop = { 
-    catalogue : products,
-    pageTitle : "Shop",
-    size : products.length,
-    activeShop : true,
-    productCSS : true
-   }
-
+  const shop = {
+    catalogue: products,
+    pageTitle: "Shop",
+    size: products.length,
+    activeShop: true,
+    productCSS: true,
+  };
 
   res.render("shop", shop);
 });
