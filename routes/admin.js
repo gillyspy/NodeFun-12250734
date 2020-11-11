830e9df4937f55f17eeabe9dad1eccaa6b21b5e5
gillyspy <gillyspy@gmail.com>
2020-11-10 19:58:04 -0500

    template replacement for shop page

--- routes/admin.js
+++ routes/admin.js
@@ -5,13 +5,17 @@ const rootDir = require('../util/path.js');
 
 const router = express.Router();
 
+const products = [];
+
 router.get('/add-product', (req, res, next)=>{
     console.log('add-product');
     res.sendFile( path.join(rootDir,'views','add-product.html'));
 });
 router.post('/add-product', (req, res, next)=>{
-    console.log('perhaps adding product in the database?', req.body);    
+    products.push( { title : req.body.title } );
+    console.log('perhaps adding product in the database?', products);    
     res.redirect('/')
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
