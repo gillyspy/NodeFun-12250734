ccd777766330e17e4928c5349dc6371459266782
gillyspy <gillyspy@gmail.com>
2020-11-21 22:16:07 -0500

    data storage in files

--- controllers/products.js
+++ controllers/products.js
@@ -13,26 +13,26 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-    const product = new Product(req.body.title, req.body.description);
-    product.save();
+  const product = new Product(req.body.title, req.body.description);
+  product.save();
   res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
-    const products = Product.fetchAll()
+  const products = Product.fetchAll((products) => {
+    const shop = {
+      catalogue: products,
+      pageTitle: 'Shop',
+      path: '/',
+      size: products.length,
+      activeShop: true,
+      CSS: {
+        formsCSS: true,
+        productCSS: true,
+      },
+      activeAddProduct: true,
+    };
 
-  const shop = {
-    catalogue: products,
-    pageTitle: 'Shop',
-    path: '/',
-    size: products.length,
-    activeShop: true,
-    CSS: {
-      formsCSS: true,
-      productCSS: true,
-    },
-    activeAddProduct: true,
-  };
-
-  res.render('shop', shop);
+    res.render('shop', shop);
+  });
 };
