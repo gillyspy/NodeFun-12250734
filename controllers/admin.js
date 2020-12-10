0ebcd1cc1328d0f0cfade404ffe89530f05ad8ad
gillyspy <gillyspy@gmail.com>
2020-12-10 01:26:46 -0500

    add authorization amongst admin users against product catalogue

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,19 +13,17 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  const product = new Product(
-    req.body.title,
-    req.body.description,
-    req.body.imageURL,
-    req.body.price,
-    null,
-    req.session.user._id
-    //req.body.id
-  );
-  product.save().then( ()=>{
+  const product = new Product({
+    title      : req.body.title,
+    description: req.body.description,
+    url        : req.body.imageURL,
+    price      : req.body.price,
+    userid     : req.session.user._id
+  });
+  product.save().then(isSaved => {
+    //TODO : error if not saved?
     res.redirect('/admin/add-product');
   })
-  
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -36,13 +34,14 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   Product.findById(prodId).then( (product) => {
+    // TODO: only allow edit mode if user is allowed to admin this particular product
     console.log('getEditProduct', product, editMode);
-    res.render('admin/edit-product', {
-      pageTitle      : 'Edit Product',
-      path           : '/admin/edit-product',
-      editing        : editMode,
-      product        : product,
-      CSS            : {
+    return res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path     : '/admin/edit-product',
+      editing  : editMode,
+      product  : product,
+      CSS      : {
         formsCSS: true,
       },
     });
@@ -52,30 +51,53 @@ exports.getEditProduct = (req, res, next) => {
 };
 
 exports.postEditProduct = (req, res, next) => {
-  const product = new Product(
-    req.body.title,
-    req.body.description,
-    req.body.imageURL,
-    req.body.price,
-    req.body.id
-  );
-  product.save().then( result=>{
-    res.redirect('/admin/product-list');  
-  }).catch(err=>{
+  //temporary product
+  const _product = new Product({
+    title      : req.body.title,
+    description: req.body.description,
+    url        : req.body.imageURL,
+    price      : req.body.price,
+    id         : req.body.id,
+    userid     : req.session.user._id
+  });
+  //must be able to find one that already exists
+  Product.findById(_product._id)
+    .then(product => {
+      //product.prepIdForDb( product._id );
+      if (!product) {
+        //no allowed to update
+        req.flash('error', 'Could not find product to edit');
+        return res.redirect('/admin/product-list');
+      } else if (product.userid != _product.userid) {
+        req.flash('error', 'You don\'t have permissions for that product');
+        return res.redirect('/admin/product-list');
+      }
+      //the temp products is a shadow of the real product
+      // the temporary one has the edits
+      // update the temporary one
+      return _product.save()
+    })
+    .then(result => {
+      console.log('Edit result', result);
+      res.redirect('/admin/product-list');
+    }).catch(err => {
     console.log(err);
   })
 
 };
 
 exports.postDeleteProduct = (req, res, next) => {
-  Product.deleteById(req.body.id).then( () => {
+  Product.deleteOne({
+    _id   : Product.prepIdForDb(req.body.id),
+    userid: Product.prepIdForDb(req.session.user._id)
+  }).then(() => {
     res.redirect('/admin/product-list');
-  }).catch(err=>{
+  }).catch(err => {
     console.log(err);
   })
 };
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find({userid: req.session.user._id})
     .then((products) => {
       res.render('admin/product-list', {
         catalogue      : products,
--- models/Product.js
+++ models/Product.js
@@ -1,14 +1,55 @@
 const mongodb = require('mongodb');
 const getDb = require('../util/db').getDb;
 
-class Product {
-  constructor(t, d = 'TBD', u, price, id=null,userId) {
-    this.title = t;
-    this.description = d;
-    this.url = u;
-    this.price = price;
-    this._id = id;
-    this.userid = userId
+const cProps = [
+  'title',
+  'description',
+  'url',
+  'price',
+  'id',
+  'userid'
+];
+
+const assignProps = function (n, o) {
+  cProps.forEach(k => {
+    if (k === 'id') {
+      n['_id'] = o[k];
+    }
+    n[k] = o[k];
+  })
+  return n;
+}
+
+class ProductShell {
+  constructor(t, d = 'TBD', u, price, id = null, userid) {
+    if (typeof t === 'object') {
+      assignProps(this, t);
+    } else {
+      this.title = t;
+      this.description = d;
+      this.url = u;
+      this.price = price;
+      this._id = id;
+      this.userid = userid
+    }
+    this['_id'] = this['_id'] || t._id || t._id || t.id
+  }
+}
+
+class Product
+  extends ProductShell {
+  constructor(t, d = 'TBD', u, price, id = null, userid) {
+    super(t, d = 'TBD', u, price, id = null, userid);
+  }
+
+  dbShadowWithId() {
+    return new ProductShell(this);  //= new Product('ghost');
+  }
+
+  dbShadowWithoutId() {
+    const _shadow = Object.assign({}, this.dbShadowWithId());
+    delete _shadow._id;
+    return _shadow;
   }
 
   save() {
@@ -16,27 +57,62 @@ class Product {
     let dbTran;
     if (this._id) {
       //update
-      this._id = new mongodb.ObjectId(this._id);
+      this.prepIdForDb();
+      //this._id = new mongodb.ObjectId(this._id);
       dbTran = db.collection('products').updateOne(
         {
-          _id: this._id,
+          _id: this._id
         },
         {
-          $set: this,
+          $set: this.dbShadowWithoutId(),
         }
       );
     } else {
-      dbTran = db.collection('products').insertOne(this);
+      dbTran = db.collection('products').insertOne(this.dbShadowWithoutId());
     }
     return dbTran
       .then((result) => {
-        console.log(result);
+        return result;
+        //console.log(result);
       })
       .catch((err) => {
         console.log(err);
       });
   }
 
+  prepIdForDb() {
+    //return
+    return Product.prepIdForDb(this);
+  }
+
+  static prepIdForDb(objOrId) {
+    // this will get called statically no matter what, but the object might be
+    // an instance of Product (see prototype for prepIdForDb )
+    // so check what `this` is
+    if (typeof objOrId === 'object' && objOrId instanceof Product) {
+      if (typeof objOrId._id !== 'object') {
+        objOrId._id = mongodb.ObjectId(objOrId._id);
+      }
+      return objOrId;
+    } else if (typeof objOrId._id === 'object') {
+      return objOrId._id
+      //already BSON id already
+    } else {
+      return mongodb.ObjectId(objOrId);
+    }
+  }
+
+  static compareProducts(p1, p2 = null) {
+    if (p1 === p2) {
+      return true;
+    } else if (p2 === null) {
+
+      //p1 is the other product and `this` is reference product
+      return thisp1._id.toString() === this._id.toString()
+    } else {
+      return p1._id.toString() === p2._id.toString();
+    }
+  }
 
   static fetchAll() {
     const db = getDb();
@@ -46,12 +122,33 @@ class Product {
       .toArray()
       .then((results) => {
         console.log(results);
+        results.forEach(r => {
+          r = new Product(r)
+        });
         return results;
       })
       .catch((err) => {
         console.log('Error in fetchAll', err);
       }); /* */
   }
+
+  static find(obj) {
+    const db = getDb();
+    const {_id, userid, ..._obj} = obj;
+    if (userid) {
+      let _userid = mongodb.ObjectId(userid)
+      _obj.userid = _userid
+    }
+    return db.collection('products').find(_obj).toArray().then(products => {
+      products.forEach(p => {
+        p = new Product(p)
+      })
+      return products;
+    }).catch(err => {
+      console.log('problem finding products')
+    });
+  }
+
   static findById(id) {
     const db = getDb();
     id = mongodb.ObjectId(id);
@@ -60,22 +157,29 @@ class Product {
       .find({_id: id})
       .next()
       .then((result) => {
+        if (!result) {
+          return null;
+        } else {
+          const p = new Product(result);
+          return p;
+        }
         console.log(result);
-        return result;
       })
       .catch((err) => {
         console.log(err);
       });
   }
-  static deleteById(id){
+
+  static deleteOne(obj) {
     const db = getDb();
-    id = mongodb.ObjectId(id)
-    return db.collection('products').deleteOne({ _id : id})
-    .then( ()=>{
-      console.log('Deleted!');
-    }).catch(err=>{
-      console.log(err);
-    });
+    return db.collection('products').deleteOne( obj )
+      .then(() => {
+        console.log('Deleted!');
+      }).catch(err => {
+        console.log(err);
+      });
   }
 }
+
 module.exports = Product;
+
